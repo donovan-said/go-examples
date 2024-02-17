09bc4e1ceaa128f4144bc926ed5639d2afd70a1a
Donovan Said <donovan.s.said@gmail.com>
2024-02-17 20:12:31 +0000

    Initial commit.

---
+++ algorithms/main.go
@@ -0,0 +1,16 @@
+/*
+	The examples algorithms found in here have been obtained from:
+	https://medium.com/@uzairahmed01/algorithm-and-data-structure-in-golang-2869da82723e
+*/
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(">> SORTING ALGORITHMS")
+
+	sorting_algo()
+}
